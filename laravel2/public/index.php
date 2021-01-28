2e2a056e030469d4d9d107e44d42dfa2cd1b2ff2
santii243 <72436574+santii243@users.noreply.github.com>
2021-01-28 20:11:48 +0100

    Configuracion para habilitar CORS

--- laravel2/public/index.php
+++ laravel2/public/index.php
@@ -8,6 +8,11 @@
  */
 
 define('LARAVEL_START', microtime(true));
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 1000");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
+header("Access-Control-Allow-Methods: PUT, POST, GET, OPTIONS, DELETE");
 
 /*
 |--------------------------------------------------------------------------
